65856723f48ca77c41bd6538e5a46e7d16438949
aureliayang <yangchen.cug@163.com>
2021-06-18 21:41:29 -0600

    continue updating, module test begin tomorrow

--- ecoslim_subgrid_bc.f90
+++ ecoslim_subgrid_bc.f90
@@ -142,4 +142,49 @@ module subgrid_bound
             end do
         end do
     end subroutine DEM_for_visual
+
+    subroutine read_restarts (fh1,fh2,rank,restartf,np,nind,P)
+        use mpi
+        implicit none
+
+        integer:: fh1,fh2,rank,np,nind
+        integer:: ierr,np_active,pid
+        character(200):: loadf, restartf, exitedf, logf, message
+        real(8):: P(:,:)
+
+        write(message,'(a,a,i3.3,a,a)') 'Reading particle restart File:', &
+        'Particle_restart.',rank,'.bin',new_line(' ')
+        call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                            MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+
+        ! read in full particle array as binary restart file, should name change?,
+        ! potential overwrite confusion
+        call MPI_FILE_OPEN(MPI_COMM_SELF,restartf,MPI_MODE_RDONLY, &
+                            MPI_INFO_NULL,fh2,ierr)
+
+        call MPI_FILE_READ(fh2,np_active,1,MPI_INTEGER,MPI_STATUS_IGNORE,ierr)
+        call MPI_FILE_READ(fh2,pid,1,MPI_INTEGER,MPI_STATUS_IGNORE,ierr)
+
+        if (np_active < np) then   ! check if we have particles left
+        call MPI_FILE_READ(fh2,P(1:np_active,1:nind*2+17),np_active*(nind*2+17), &
+                            MPI_DOUBLE_PRECISION,MPI_STATUS_IGNORE,ierr)
+        call MPI_FILE_CLOSE(fh2, ierr)
+
+        write(message,'(a,i10.10,a)') 'RESTART np_active:',np_active,new_line(' ')
+        call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                            MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+        write(message,'(a,i10.10,a)') 'RESTART pid:',pid,new_line(' ')
+        call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                            MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+        else
+        call MPI_FILE_CLOSE(fh2, ierr)
+        write(message,'(A,A)') ' **Warning restart IC input but no paricles left',new_line(' ')
+        call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                            MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+        write(message,'(A,A)') ' **Exiting code *not* (over)writing restart',new_line(' ')
+        call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                            MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+        stop
+        end if
+    end subroutine read_restarts
 end module subgrid_bound
