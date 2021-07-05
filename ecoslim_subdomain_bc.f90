5cbe825c6ea734ef6dd18fb3a97af44fe61cb450
aureliayang <yangchen.cug@163.com>
2021-07-05 18:01:35 -0400

    go thourh the structure again

--- ecoslim_subdomain_bc.f90
+++ ecoslim_subdomain_bc.f90
@@ -4,32 +4,43 @@ module subdomain_bound
     ! subroutine DEM_for_visual
     ! subroutine read_restarts
 
-    use variable_list, only: ppx, qqy
+    use variable_list, only: ppx, qqy, rank, buff
+    use variable_list, only: nx, ny, nz
+    use variable_list, only: dx, dy, dz
+    use variable_list, only: Xgmin, Xgmax, Ygmin, Ygmax, Zgmin, Zgmax
+    use variable_list, only: Xmin, Xmax, Ymin, Ymax, Zmin, Zmax
+    use variable_list, only: loadf, restartf, exitedf, logf
+    use variable_list, only: fh1, fh2
+    use variable_list, only: np, nind, np_active, pid
+    use variable_list, only: P
+    use variable_list, only: nnx1, nny1
 
 contains
-    subroutine global_xyz(nx, ny, nz, dx, dy, dz, rank, &
-        Xmin, Xmax, Ymin, Ymax, Zmin, Zmax, fh1, &
-        loadf, restartf, exitedf, logf)
+    subroutine global_xyz()
+        ! get the boundary of the whole domain.
+        ! set the name of log files
+        ! open loadf (fh1) and output the domain boundary to loadf
+
+        ! loadf (fh1) looks like the log file, recodring all the warning info
+        ! restartf (fh2) is used to read and write restart files
+        ! exitedf (fh3) is to write files of exited particles, it includes particles for all timesteps.
+        ! logf (fh4) is also log file to record the summary of particles of every timestep
 
         use mpi
         implicit none
-
-        integer:: nx, ny, nz, rank
-        real(8):: dx, dy, dz(:)
-        real(8):: Xmin, Xmax, Ymin, Ymax, Zmin, Zmax
-        character(200):: loadf, restartf, exitedf, logf, message
-        integer:: k, ierr, fh1
+        character(200):: message
+        integer:: k, ierr
 
         if (rank /= ppx*qqy) then
             ! set up domain boundaries
-            Xmin = 0.0d0
-            Ymin = 0.0d0
-            Zmin = 0.0d0
-            Xmax = dble(nx)*dx
-            Ymax = dble(ny)*dy
-            Zmax = 0.0d0
+            Xgmin = 0.0d0
+            Ygmin = 0.0d0
+            Zgmin = 0.0d0
+            Xgmax = dble(nx)*dx
+            Ygmax = dble(ny)*dy
+            Zgmax = 0.0d0
             do k = 1, nz
-                Zmax = Zmax + dz(k)
+                Zgmax = Zgmax + dz(k)
             end do
 
             write(loadf,'(a,i3.3,a)') 'Load_info.', rank, '.txt'
@@ -48,48 +59,45 @@ contains
             call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
             MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
 
-            write(message,'("Xmin:",e12.5," Xmax:",e12.5,A)') Xmin, Xmax, NEW_LINE(' ')
+            write(message,'("Xmin:",e12.5," Xmax:",e12.5,A)') Xgmin, Xgmax, NEW_LINE(' ')
             call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
             MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
 
-            write(message,'("Ymin:",e12.5," Ymax:",e12.5,A)') Ymin, Ymax, NEW_LINE(' ')
+            write(message,'("Ymin:",e12.5," Ymax:",e12.5,A)') Ygmin, Ygmax, NEW_LINE(' ')
             call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
             MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
 
-            write(message,'("Zmin:",e12.5," Zmax:",e12.5,A)') Zmin, Zmax, NEW_LINE(' ')
+            write(message,'("Zmin:",e12.5," Zmax:",e12.5,A)') Zgmin, Zgmax, NEW_LINE(' ')
             call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
             MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+
         endif
 
     end subroutine global_xyz
 
-    subroutine local_xyz(nnx1, nny1, nz, dx, dy, dz, buff, &
-        Xmin, Xmax, Ymin, Ymax, Zmin, Zmax)
-        use mpi
+    subroutine local_xyz()
         implicit none
+        integer:: k
 
-        integer:: nnx1, nny1, nz, k
-        real(8):: dx, dy, dz(:), buff
-        real(8):: Xmin, Xmax, Ymin, Ymax, Zmin, Zmax
-
-        ! set up subdomain boundaries
-        Xmin = -dble(buff)*dx
-        Ymin = -dble(buff)*dy
-        Zmin = 0.0d0
-        Xmax = dble(nnx1)*dx + dble(buff)*dx
-        Ymax = dble(nny1)*dy + dble(buff)*dy
-        Zmax = 0.0d0
-        do k = 1, nz
-            Zmax = Zmax + dz(k)
-        end do
-        ! need output to log file
-        ! return the global and local info of the subdomain
-        ! add grid info to log file
-        ! write(message,'(a,a,i5,a,4(i10,1x),a)') new_line(' '),'rank:',rank, &
-        ! ', Gridinfo (ix1,iy1,nnx1,nny1):',grid(rank+1,1:4),new_line(' ')
-        ! call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
-        ! MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
-
+        if (rank /= ppx*qqy) then
+            ! set up subdomain boundaries
+            Xmin = -dble(buff)*dx
+            Ymin = -dble(buff)*dy
+            Zmin = 0.0d0
+            Xmax = dble(nnx1)*dx + dble(buff)*dx
+            Ymax = dble(nny1)*dy + dble(buff)*dy
+            Zmax = 0.0d0
+            do k = 1, nz
+                Zmax = Zmax + dz(k)
+            end do
+            ! need output to log file
+            ! return the global and local info of the subdomain
+            ! add grid info to log file
+            ! write(message,'(a,a,i5,a,4(i10,1x),a)') new_line(' '),'rank:',rank, &
+            ! ', Gridinfo (ix1,iy1,nnx1,nny1):',grid(rank+1,1:4),new_line(' ')
+            ! call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+            ! MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+        end if
     end subroutine local_xyz
 
     subroutine DEM_for_visual(DEM,DEMname,ix1,iy1,nnx1,nny1,nz)
@@ -163,49 +171,48 @@ contains
         end do
     end subroutine DEM_for_visual
 
-    subroutine read_restarts (fh1,fh2,rank,restartf,np,nind,P)
+    subroutine read_restarts ()
         use mpi
         implicit none
 
-        integer,intent(in):: fh1, rank, np, nind
-        integer,intent(out):: fh2, np_active, pid
         integer:: ierr
-        character(200):: restartf, message
-        real(8),intent(out):: P(:,:)
+        character(200):: message
 
-        write(message,'(a,a,i3.3,a,a)') 'Reading particle restart File:', &
-        'Particle_restart.',rank,'.bin',new_line(' ')
-        call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
-                            MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+        if (rank /= ppx*qqy) then
 
-        ! read in full particle array as binary restart file, should name change?,
-        ! potential overwrite confusion
-        call MPI_FILE_OPEN(MPI_COMM_SELF,restartf,MPI_MODE_RDONLY, &
-                            MPI_INFO_NULL,fh2,ierr)
-
-        call MPI_FILE_READ(fh2,np_active,1,MPI_INTEGER,MPI_STATUS_IGNORE,ierr)
-        call MPI_FILE_READ(fh2,pid,1,MPI_INTEGER,MPI_STATUS_IGNORE,ierr)
-
-        if (np_active < np) then   ! check if we have particles left
-            call MPI_FILE_READ(fh2,P(1:np_active,1:nind*2+17),np_active*(nind*2+17), &
-                                MPI_DOUBLE_PRECISION,MPI_STATUS_IGNORE,ierr)
-            call MPI_FILE_CLOSE(fh2, ierr)
-
-            write(message,'(a,i10.10,a)') 'RESTART np_active:',np_active,new_line(' ')
+            write(message,'(a,a,i3.3,a,a)') 'Reading particle restart File:', &
+            'Particle_restart.',rank,'.bin',new_line(' ')
             call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
                                 MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
-            write(message,'(a,i10.10,a)') 'RESTART pid:',pid,new_line(' ')
-            call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
-                                MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
-        else
-            call MPI_FILE_CLOSE(fh2, ierr)
-            write(message,'(A,A)') ' **Warning restart IC input but no paricles left',new_line(' ')
-            call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
-                                MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
-            write(message,'(A,A)') ' **Exiting code *not* (over)writing restart',new_line(' ')
-            call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
-                                MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
-            stop
+
+            call MPI_FILE_OPEN(MPI_COMM_SELF,restartf,MPI_MODE_RDONLY, &
+                                MPI_INFO_NULL,fh2,ierr)
+
+            call MPI_FILE_READ(fh2,np_active,1,MPI_INTEGER,MPI_STATUS_IGNORE,ierr)
+            call MPI_FILE_READ(fh2,pid,1,MPI_INTEGER,MPI_STATUS_IGNORE,ierr)
+
+            if (np_active < np) then   ! check if we have particles left
+                call MPI_FILE_READ(fh2,P(1:np_active,1:nind*2+17),np_active*(nind*2+17), &
+                                    MPI_DOUBLE_PRECISION,MPI_STATUS_IGNORE,ierr)
+                call MPI_FILE_CLOSE(fh2, ierr)
+
+                write(message,'(a,i10.10,a)') 'RESTART np_active:',np_active,new_line(' ')
+                call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                                    MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+                write(message,'(a,i10.10,a)') 'RESTART pid:',pid,new_line(' ')
+                call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                                    MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+            else
+                call MPI_FILE_CLOSE(fh2, ierr)
+                write(message,'(A,A)') ' **Warning restart IC input but no paricles left',new_line(' ')
+                call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                                    MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+                write(message,'(A,A)') ' **Exiting code *not* (over)writing restart',new_line(' ')
+                call MPI_FILE_WRITE(fh1, trim(message), len(trim(message)), &
+                                    MPI_CHARACTER, MPI_STATUS_IGNORE, ierr)
+                stop
+            end if
+
         end if
     end subroutine read_restarts
 end module subdomain_bound
