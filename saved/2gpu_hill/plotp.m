06189d07e95594d820ea643cec25a24eead9036e
aureliayang <yangchen.cug@163.com>
2021-09-28 18:25:31 -0400

    add README.md for most of the folders

--- saved/2gpu_hill/plotp.m
+++ saved/2gpu_hill/plotp.m
@@ -1,13 +1,14 @@
 clear;
 gpu1=load('Log_particles.00000.txt');
 gpu2=load('Log_particles.00001.txt');
+gpu=load('../1gpu_hill/Log_particles.00000.txt');
 h1=load('../half1.txt');
 h2=load('../half2.txt');
 hh(1:99999)=h1(1:99999,15);
 hh(100000:175200)=h2(1:75201,14);
 
 t=1:175200;
-ll=plot(t,gpu1(t,14)+gpu2(t,14),t,hh,'--',t,gpu1(t,14),t,gpu2(t,14));
+ll=plot(t,gpu1(t,14)+gpu2(t,14),t,hh,'--',t,gpu1(t,14),t,gpu2(t,14),1:length(gpu),gpu(:,14));
 set(ll,'linewidth',2);
 set(gca,'FontSize',18,'FontName','Times New Roman','FontWeight','Bold','Linewidth',1);
 ylabel('Active Particle Number','FontSize',20,'FontName','Times New Roman','FontWeight','Bold');
---
+++ saved/README.md
@@ -0,0 +1,18 @@
+1gpu_hill: it is based on hillslope model using 1 GPU
+
+1gpu_ideal: the ideal case, square
+
+2gpu_hill: it is based on hillslope model using 2 GPUs
+
+4gpu_ideal: the ideal case, square
+
+4gpu_LW: it is based on the LW case
+
+4gpu_LW2: it is similar to 4gpu_LW. The difference is there is edition in the compaction kernel.
+
+profiling: it is the preliminary profling results for LW.
+
+SLIM_hillslope_ER_shrub_log.txt and SLIM_hillslope_ER_shrub_PET_balance.txt are from cpu version EcoSLIM
+half1.txt and half2.txt are the two parts of SLIM_hillslope_ER_shrub_log.txt
+
+
